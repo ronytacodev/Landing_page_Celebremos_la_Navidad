ca3ea472098f8f56426795e47bb7fbeab0fb0350
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-11-12 10:54:02 -0500

    modificando en js, el ícono para la barra de búsqueda

--- js/app.js
+++ js/app.js
@@ -3,11 +3,18 @@ let searchBtn = document.querySelector('#search-btn');
 let searchForm = document.querySelector('.header .search-form');
 
 searchBtn.onclick = ()=> {
-    searchBtn.classList.toggle('fa-times');
+    searchBtn.classList.toggle('fa-xmark');
     searchForm.classList.toggle('active');
 
-    menubar.classList.remove('fa-times');
+    menubar.classList.remove('fa-xmark');
     navbar.classList.remove('active');
 }
 // Fin Barra de Búsqueda
+
+window.onscroll = () => {
+    searchBtn.classList.remove('fa-xmark');
+    searchForm.classList.remove('active');
+    menubar.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+}
 
